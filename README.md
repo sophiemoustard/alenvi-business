ca35d165afd4e89d36d8618e7f49d154c3c24ce1
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-09-07 11:50:30 +0200

    Add basic Quasar-Framework project

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# alenvi-business
-Alenvi business website made with Quasar Framework
+# Quasar App
+
+> A Quasar project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:8080
+$ quasar dev
+
+# build for production with minification
+$ quasar build
+
+# lint code
+$ quasar lint
+```
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const history = require('connect-history-api-fallback');
+
+app.use(history());
+
+app.use(express.static(__dirname + '/dist'));
+
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'), () => {
+  console.log(`Server launched on ${process.env.port || 8080}`);
+});
