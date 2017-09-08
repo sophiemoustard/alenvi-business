797b341b8392bda4f112c78064f3fb0bd043ac53
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-09-08 10:24:47 +0200

    Add name and ttile properties to routes

--- src/router.js
+++ src/router.js
@@ -23,12 +23,8 @@ export default new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: load('Hello') },
-<<<<<<< HEAD
-    { path: '/test', component: load('test') },
-
-=======
-    { path: '/home', component: load('Home/Home') },
->>>>>>> cfd909b53ff33a45bf209266a4c2ec4cb47609ab
+    { path: '/home', name: 'home', title: 'Accueil', component: load('Home/Home') },
+    { path: '/test', name: 'test', title: 'Test', component: load('Layout/Test') },
     // Always leave this last one
     { path: '*', component: load('Error404') } // Not found
   ]
