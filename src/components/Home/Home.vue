08f5d554f16bdc8764d592fcd00e8f3fd42f753e
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-09-11 12:03:31 +0200

    Better version of homepage

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,32 +1,32 @@
 <template lang="html">
-  <main class="layout-padding">
+  <main class="">
     <!-- main presentation -->
-    <section id="main-presentation" class="row justify-center section-padding">
-      <div class="col-sm-5">
+    <section id="main-presentation" class="row sm-gutter section-margin">
+      <div id="main-text-and-call" class="self-center col-sm-5">
         <h1 class="main-section-title">Accompagner le quotidien, stimuler les envies.</h1>
         <h2 class="main-section-subtitle">Nous sommes des professionnels passionnés par notre métier. Nous créons des liens sur le long terme avec les personnes âgées.</h2>
         <q-btn>
           Appeler au 01 79 75 54 75
         </q-btn>
       </div>
-      <div class="gallery-cropper col-sm-7">
+      <div class="gallery-param col-sm-7">
         <q-gallery-carousel class="gallery-cropper" infinite autoplay :src="gallery"></q-gallery-carousel>
         <!-- <div class="half-background"></div> -->
       </div>
     </section>
     <!-- features -->
-    <section id="features" class="row justify-center section-padding">
-      <div class="col-4" align="center">
+    <section id="features" class="row layout-padding justify-center section-padding">
+      <div class="col-sm-4" align="center">
         <img class="image-center" src="statics/picto.png" alt="">
         <h5>Aide à domicile</h5>
         <p>Nous assistons votre proche pour les tâches de la vie quotidienne et passons de bons moments ensemble.</p>
       </div>
-      <div class="col-4" align="center">
+      <div class="col-sm-4" align="center">
         <img class="image-center" src="statics/picto.png" alt="">
         <h5>Compagnie & stimulation</h5>
         <p>Formés en continu sur les troubles cognitifs, nous sommes là pour aider votre proche à garder des activités : promenades, jeux...</p>
       </div>
-      <div class="col-4" align="center">
+      <div class="col-sm-4" align="center">
         <img class="image-center" src="statics/picto.png" alt="">
         <h5>Sortie d'hospitalisation</h5>
         <p>Nous aidons votre proche à retrouver son autonomie lorsqu’il regagne son domiciille après un passage à l’hôpital.</p>
@@ -50,8 +50,8 @@ export default {
   data() {
     return {
       gallery: [
-        'statics/companionship.jpg',
-        'statics/companionship.jpg'
+        'statics/home_care.jpg',
+        'statics/home_care.jpg'
       ]
     }
   }
@@ -60,7 +60,7 @@ export default {
 
 <style lang="css" scoped>
 #main-presentation {
-  /*background-color: blue;*/
+  background-color: #FAF9F8;
 }
 
 h1, h2, h3, h4, h5, h6 {
@@ -69,37 +69,20 @@ h1, h2, h3, h4, h5, h6 {
 
 .main-section-title {
   font-size: 2.8em;
-  text-align: center;
+  /*text-align: center;*/
 }
 
 .main-section-subtitle {
   font-size: 1.5em;
-  text-align: center;
+  /*text-align: center;*/
 }
 
 .gallery-cropper {
-  width: 100%;
+  /*width: 100%;
   height: auto;
-  /*max-width: 90vw;*/
-  /*border-top: 80px solid white;
-  border-left: 80px solid red;*/
-
-  /*background: red;*/
-  /*position: relative;*/
+  max-width: 90vw;*/
 }
 
-.gallery-cropper:before {
-  /*content: '';
-  border-top: 80px solid white;
-  border-left: 80px solid red;*/
-  /*position: absolute;*/
-  /*top: 0; right: 0;*/
-  /*width: 0;*/
-   /*display: inline;
-   margin: 0 auto;
-   height: 100%;
-   width: auto;*/
-}
 .half-background {
   top: 0;
   left: 0;
@@ -114,12 +97,17 @@ h1, h2, h3, h4, h5, h6 {
   position: absolute;
   background-color: #faf9f8;
 }
-.section-padding {
-  padding-bottom: 60px;
-}
+/*.section-margin {
+  margin-bottom: 3%;
+}*/
 .image-center {
   width: 100px;
   height: 100px;
+}
+
+#main-text-and-call {
+  padding-left: 5%;
+  padding-right: 5%;
 }
 
 </style>
