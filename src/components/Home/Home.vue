a9daa472c5871e343459c649209ad74ad178b8af
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-09-11 17:27:06 +0200

    Home page big improves

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -2,34 +2,49 @@
   <main class="">
     <!-- main presentation -->
     <section id="main-presentation" class="row sm-gutter section-margin">
-      <div id="main-text-and-call" class="self-center col-sm-5">
+      <div id="main-text-and-call" class="self-center col-md-5">
         <h1 class="main-section-title">Accompagner le quotidien, stimuler les envies.</h1>
         <h2 class="main-section-subtitle">Nous sommes des professionnels passionnés par notre métier. Nous créons des liens sur le long terme avec les personnes âgées.</h2>
-        <q-btn>
-          Appeler au 01 79 75 54 75
+        <q-btn color="primary">
+          <a class="btn-call" href="tel:+33179755475">Appeler le 01 79 75 54 75</a>
         </q-btn>
       </div>
-      <div class="gallery-param col-sm-7">
+      <div class="col-md-7">
         <q-gallery-carousel class="gallery-cropper" infinite autoplay :src="gallery"></q-gallery-carousel>
         <!-- <div class="half-background"></div> -->
       </div>
     </section>
     <!-- features -->
-    <section id="features" class="row layout-padding justify-center section-padding">
-      <div class="col-sm-4" align="center">
-        <img class="image-center" src="statics/picto.png" alt="">
-        <h5>Aide à domicile</h5>
-        <p>Nous assistons votre proche pour les tâches de la vie quotidienne et passons de bons moments ensemble.</p>
+    <section id="features" class="row layout-padding section-margin">
+      <div class="col-sm-4">
+        <div class="align-center">
+          <img class="main-picto" src="statics/picto.png" alt="">
+          <h5>Aide à domicile</h5>
+        </div>
+        <p class="feature-text">Nous assistons votre proche pour les tâches de la vie quotidienne et passons de bons moments ensemble.</p>
+        <div class="align-center">
+          <a href="#">En savoir plus</a>
+        </div>
       </div>
-      <div class="col-sm-4" align="center">
-        <img class="image-center" src="statics/picto.png" alt="">
-        <h5>Compagnie & stimulation</h5>
-        <p>Formés en continu sur les troubles cognitifs, nous sommes là pour aider votre proche à garder des activités : promenades, jeux...</p>
+      <div class="col-sm-4">
+        <div class="align-center">
+          <img class="main-picto" src="statics/picto.png" alt="">
+          <h5>Compagnie & stimulation</h5>
+        </div>
+        <p class="feature-text">Formés en continu sur les troubles cognitifs, nous sommes là pour aider votre proche à garder des activités : promenades, jeux...</p>
+        <div class="align-center">
+          <a href="#">En savoir plus</a>
+        </div>
       </div>
-      <div class="col-sm-4" align="center">
-        <img class="image-center" src="statics/picto.png" alt="">
-        <h5>Sortie d'hospitalisation</h5>
-        <p>Nous aidons votre proche à retrouver son autonomie lorsqu’il regagne son domiciille après un passage à l’hôpital.</p>
+      <div class="col-sm-4">
+        <div class="align-center">
+          <img class="main-picto" src="statics/picto.png" alt="">
+          <h5>Sortie d'hospitalisation</h5>
+        </div>
+        <p class="feature-text">Nous aidons votre proche à retrouver son autonomie lorsqu’il regagne son domiciille après un passage à l’hôpital.</p>
+        <div class="align-center">
+          <a href="#">En savoir plus</a>
+        </div>
       </div>
     </section>
   </main>
@@ -50,8 +65,10 @@ export default {
   data() {
     return {
       gallery: [
-        'statics/home_care.jpg',
-        'statics/home_care.jpg'
+        'statics/home_care_1.jpg',
+        'statics/home_care_2.jpg',
+        'statics/home_care_3.jpg',
+        'statics/home_care_4.jpg'
       ]
     }
   }
@@ -59,12 +76,14 @@ export default {
 </script>
 
 <style lang="css" scoped>
+
 #main-presentation {
   background-color: #FAF9F8;
 }
 
-h1, h2, h3, h4, h5, h6 {
-  font-weight: 400;
+#main-text-and-call {
+  padding-left: 5%;
+  padding-right: 5%;
 }
 
 .main-section-title {
@@ -81,13 +100,17 @@ h1, h2, h3, h4, h5, h6 {
   /*width: 100%;
   height: auto;
   max-width: 90vw;*/
+  width: 100%;
+  height: auto;
+  max-height: 500px;
 }
 
 .half-background {
+  /*z-index: -1;
   top: 0;
   left: 0;
   width: 100%;
-  height: 200%;
+  height: 100%;
   margin-left: -50%;
   margin-top: -15%;
   webkit-transform: rotate(8deg);
@@ -95,19 +118,26 @@ h1, h2, h3, h4, h5, h6 {
   transform: rotate(8deg);
   display: block;
   position: absolute;
-  background-color: #faf9f8;
+  background-color: #faf9f8;*/
 }
-/*.section-margin {
+
+.section-margin {
   margin-bottom: 3%;
-}*/
-.image-center {
+}
+
+.main-picto {
   width: 100px;
   height: 100px;
 }
 
-#main-text-and-call {
-  padding-left: 5%;
-  padding-right: 5%;
+.align-center {
+  text-align: center;
+}
+
+.feature-text {
+  text-align: justify;
+  padding-left: 10%;
+  padding-right: 10%;
 }
 
 </style>
