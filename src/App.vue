3b82fe28c2d962812d990bf776f2a3f40ceb4721
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-09-13 13:22:13 +0200

    Add alenvi color for fonts

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,13 @@ export default {
 </script>
 
 <style lang="stylus">
+@import '~variables'
+
 body
   font-family: alenviTextFont
+  color: $tertiary
 
 h1, h2, h3, h4, h5, h6
   font-family: alenviTitleFont
+  color: $tertiary
 </style>
