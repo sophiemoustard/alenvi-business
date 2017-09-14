dca266148aad88cb67714c59c92b3b1e96bb5be0
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-09-14 14:57:50 +0200

    Fix carousel component responsiveness

--- src/components/CarouselPress/CarouselPress.vue
+++ src/components/CarouselPress/CarouselPress.vue
@@ -3,17 +3,17 @@
     <q-carousel infinite autoplay class="text-tertiary">
       <div v-for="slide in slides" slot="slide" class="bg-carousel-transparent">
         <div class="row">
-          <div v-for="content in slide" class="col-xs-12 col-sm-6 col-md-6">
+          <div v-for="content in slide" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
             <a :href="content.link">
               <q-card color="white" class="shadow-1">
-                <div class="carousel-card row items-center justify-center content-center">
-                  <div class="col-lg-4">
+                <div class="carousel-card row items-center content-between">
+                  <div class="col-xs-12 col-sm-12 col-md-4 img-carousel">
                     <div class="row justify-center">
                       <img :src="content.img" :alt="content.imgAlt">
                     </div>
                   </div>
-                  <div class="col-lg-8 content">
-                    <div class="row content-center">
+                  <div class="col-xs-12 col-sm-12 col-md-8 content">
+                    <div class="row justify-center">
                       <div class="text-italic">
                         {{ content.description }}
                       </div>
@@ -61,6 +61,22 @@ export default {
             description: 'Dimpiek ru nuibekic bihipi alufuhu losiz pahhe daacegi viufook arocub vevsees osbewgu cota.',
             link: 'https://www.leparisien.fr'
           }
+        ],
+        [
+          {
+            title: 'CNews',
+            img: 'statics/cnews.png',
+            imgAlt: 'cnews logo',
+            description: 'Turihkew vafom vesiujo iw gopzu defru nobocif novo ci tu mabnos pafik zibhu oswu oso den gob favhuki.',
+            link: 'https://www.cnews.fr'
+          },
+          {
+            title: 'Le parisien',
+            img: 'statics/leparisien.png',
+            imgAlt: 'leparisien logo',
+            description: 'Dimpiek ru nuibekic bihipi alufuhu losiz pahhe daacegi viufook arocub vevsees osbewgu cota.',
+            link: 'https://www.leparisien.fr'
+          }
         ]
       ]
     }
@@ -70,6 +86,10 @@ export default {
 
 <style lang="stylus">
   @import '~variables'
+
+  @media (max-width: 1024px)
+    .img-carousel
+      margin: 10px 0px 10px 0px
 
   .bg-carousel-gradient
     background: linear-gradient(135deg, $grey-carousel 0%, $grey-carousel-fade-1 20%, $grey-carousel-fade-2 40%, $white 50%)
@@ -89,4 +109,8 @@ export default {
   .content
     color: $tertiary
     font-weight: 100
+
+    @media (max-width: 1024px)
+      padding: 0px 0px 10px 15px
+
 </style>
