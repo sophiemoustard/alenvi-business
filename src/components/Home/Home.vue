1950ef09b4db8425054cf41bf4b89e8e01b7c3cc
Pierre HOFMAN <pierre.hofman@epitech.eu>
2017-09-18 17:08:57 +0200

    Integrate LatestArticles and beautifullied a bit Home.vue

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,16 +1,19 @@
 <template lang="html">
   <main class="">
     <section id="main-presentation">
-      <main-presentation></main-presentation>
+      <main-presentation />
     </section>
     <section id="features" class="section-margin">
-      <features></features>
+      <features />
     </section>
     <section id="auxiliaries-gallery" class="section-margin">
-      <auxiliaries-gallery></auxiliaries-gallery>
+      <auxiliaries-gallery />
     </section>
     <section id="carousel-media">
-      <carousel-media></carousel-media>
+      <carousel-media />
+    </section>
+    <section id="latest-articles">
+      <latest-articles />
     </section>
   </main>
 </template>
@@ -20,14 +23,16 @@
 import MainPresentation from './MainPresentation.vue'
 import Features from './Features.vue'
 import AuxiliariesGallery from '../AuxiliariesGallery.vue'
-import CarouselMedia from '../CarouselMedia/CarouselMedia.vue'
+import CarouselMedia from './CarouselMedia.vue'
+import LatestArticles from './LatestArticles.vue'
 
 export default {
   components: {
     MainPresentation,
     CarouselMedia,
     AuxiliariesGallery,
-    Features
+    Features,
+    LatestArticles
   }
 }
 </script>
