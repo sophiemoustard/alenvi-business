6b60626d63b259a74f2554b2c1cb8f7551a02da1
Jean-Christophe TREBALAG <jean-christophetrebalag@MacBook-Pro-de-Jean-Christophe.local>
2017-09-21 11:12:24 +0200

    Adjusting landing pages display + adding gradient background

--- src/components/LandingPages/HomeCare.vue
+++ src/components/LandingPages/HomeCare.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="row">
+  <div class="row bg-gradient">
     <breadcrumb class="breadcrumb"/>
     <div class="row justify-center title">
       <h1>Aide à domicile pour les personnes âgées</h1>
     </div>
     <section class="content">
-      <div class="row items-start lg-gutter layout-padding">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+      <div class="row items-start xl-gutter layout-padding">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-xl-8">
           <h2>Françoise et Sabine</h2>
           <p class="text-justify">
             Tous les matins, Sabine retrouve Françoise à son domicile. Après un petit café, Sabine aide Françoise à faire un brin de toilette. La préparation du repas est ensuite l’occasion de passer un bon moment ensemble. Sabine est contente de voir que Françoise apprécie le plat cuisiné ensemble. Elle la laisse ensuite prendre un peu de repos en attendant la visite de sa fille dans l’après-midi.
@@ -18,7 +18,7 @@
             Nous sommes à la disposition de votre proche âgé pour l’aider dans ses gestes de la vie quotidienne et passer du temps de qualité avec lui.
           </p>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
           <img class="img-home-care" src="statics/aide_domicile.jpg" alt="photo aide-a-domicile">
         </div>
       </div>
@@ -46,6 +46,9 @@ export default {
 
   // h1
     // padding-top: 3%
+
+  .bg-gradient
+    background: linear-gradient(180deg, $grey-carousel-fade-1, $white 90%)
 
   .breadcrumb
     padding-left: 1%
