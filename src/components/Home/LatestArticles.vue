4d6978735a86d9a954080111ad513b9c28a146d7
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-10-04 10:44:21 +0200

    Rss feed ok

--- src/components/Home/LatestArticles.vue
+++ src/components/Home/LatestArticles.vue
@@ -8,10 +8,10 @@
     <div v-for="article in lastArticles" class="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-xl-4">
       <q-card class="card" flat>
         <q-card-media>
-          <img class="img-articles" src="https://res.cloudinary.com/alenvi/image/upload/q_auto/v1507019475/images/business/home_care_1.jpg" alt="photo article">
+          <img class="img-articles" :src="article.image.link" alt="photo article">
         </q-card-media>
         <q-card-title>
-          {{ article.title | truncate('30') }}
+          {{ article.title | truncate('50') }}
         </q-card-title>
         <q-card-main class="card-content">
           {{ article.content | truncate('100') }}
@@ -35,7 +35,7 @@ import {
   QIcon
 } from 'quasar'
 
-import Feedparser from 'feedparser'
+import _ from 'lodash'
 
 export default {
   components: {
@@ -57,47 +57,50 @@ export default {
   },
   data () {
     return {
-      lastArticles: [
-        {
-          img: '',
-          title: 'Article 1',
-          content: 'Napo mocdo iv eh mit sal izvamip izjijfef taacamoc ku neosomuk agugowol puv sejalam hibtu nuhfegel hi. Tidjig oc lijo sap pefofca je raudze wimosoke adajif poir nadup kef gocji cotozek fo adhooja tivosa. Mocko nollam pefutu donsemze tescu bowdus mojmot emi lil er gofdet lu di. Ek imutzu pieka bozem lob bedfibu migih zaf eme oj icafuic omodagpo evtok jozti. Nugiw gofrim pe ta kofev wephapu lu lajugnu voctu wejago ij pohiilo lenva turci no. Sotvinzuz irama hume otjet altighuc gadogic go fakumu pom opokuzko ima velvu ko besaumi karwi ihiisa. Fuj isvejlo naoz dage rijvur olujesmab leogtal wulize mi zobonmoz ok ekoso tonan ejuritabe itoin ed.'
-        },
-        {
-          img: '',
-          title: 'Article 2',
-          content: 'Lutaka otmaliv paj vazcif paavi jacefube vawujpem dezasik ecife huroez dukwab bomcimat irpara. Ti sav woji bubonsus lezwis natereg raf fuper mi fes up fepragkuw ra bojivfuf boti wih volmik. Ufcobo oj mug ki mo surwaw vettodbo pa ive nes me ji okpinfo pumed. Licuj udvoj aw be erca butruoh mogvoc gu kopke sorza higuv terteg su famdu tik dop gow dacocdih. Lof sevtek bovu vam jorpuwlat bo co edo keaco arsi ica ejlutdof reru egbip.'
-        },
-        {
-          img: '',
-          title: 'Article 3',
-          content: 'Haalu huzi mo ka hu zawlad me ez mepi senpaj ji usbepwew lekojwig ticculi do pe seruh evdeb. Miedpi hawkef rucemun suhtipdo su ishe fozre nukjoj nalzeec kigaw fawo emlu fibiku zaw ittih ub. Li pobnehaki cifhinli zedcus lisok ved wovcikih di gur wa fefi onhahic insevek keru.'
-        }
-      ]
+      lastArticles: []
     }
   },
   async created () {
-    try {
-      var req = await this.$http('https://blog.alenvi.io/rss/');
-      var feedparser = new Feedparser();
-      var stream = this;
-      stream.pipe(feedparser);
+    const feeds = await this.$http.get('https://93f62618.ngrok.io/blog/rssFeeds', {
+      params: {
+        feed_url: 'https://blog.alenvi.io/rss/'
+      }
+    });
+    const orderedFeeds = _.sortBy(feeds.data.data, ['pubDate']).reverse().slice(0,3);
+    orderedFeeds.forEach(item => {
+      let article = {
+        title: item.title,
+        content: item.summary,
+        image: {
+          link: this.getImgInfo(item.description, 'src'),
+          alt: this.getImgInfo(item.description, 'alt'),
+        },
+        link: item.link
+      };
+      this.lastArticles.push(article);
+    });
+    // console.log(this.lastArticles);
+  },
+  methods: {
+    getImgInfo (text, type) {
+      let regex;
+      switch (type) {
+        case 'src':
+          regex = /<img src="(.*?)"/;
+          break;
+        case 'alt':
+          regex = /<img src=".*?" alt="(.*?)"/;
+          break;
+      }
 
-      feedparser.on('error', function (error) {
-        throw new Error ('Feedparser error')
-      });
+      const found = text.match(regex);
+      // console.log(found);
 
-      feedparser.on('readable', function () {
-        var stream = this; // `this` is `feedparser`, which is a stream
-        var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-        var item;
-
-        while (item = stream.read()) {
-          console.log(item);
-        }
-      });
-    } catch (e) {
-      console.error(e);
+      if (found) {
+        return found[1];
+      } else {
+        return null;
+      }
     }
   }
 }
@@ -108,8 +111,8 @@ export default {
 
 .img-articles
   width: 100%
-  height: auto
-  
+  height: 275px
+
 .q-card-actions a:hover
   color: #ff1693 !important
 </style>
