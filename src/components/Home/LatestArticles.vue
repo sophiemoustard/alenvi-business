d11ff30d8dbf7a11d1000f8182093e9b1dc3e928
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2017-10-09 17:07:14 +0200

    Media queries added for latest articles images

--- src/components/Home/LatestArticles.vue
+++ src/components/Home/LatestArticles.vue
@@ -5,7 +5,7 @@
         <h1>Blog : les derniers articles</h1>
       </div>
     </div>
-    <div v-for="article in lastArticles" class="col-xs-12 col-sm-4">
+    <div v-for="(article) in lastArticles" class="col-xs-12 col-sm-4">
       <q-card class="card" flat>
         <q-card-media>
           <img class="img-articles" :src="article.image.link" alt="photo article">
@@ -81,6 +81,11 @@ export default {
     });
     // console.log(this.lastArticles);
   },
+  // mounted() {
+  //   this.$nextTick(() => {
+  //     console.log(this.$refs['article1'].clientHeight)
+  //   })
+  // },
   methods: {
     getImgInfo (text, type) {
       let regex;
@@ -111,11 +116,21 @@ export default {
 
 .img-articles
   width: 100%
-  height: 325px
-  @media (max-width: 400px)
-    height: 220px
-  @media (min-width: 1600px)
-    height: 400px
+  height: 250px
+  @media (min-width: 320px) and (max-width: 400px)
+    height: 199px
+  @media (min-width: 401px) and (max-width: 600px)
+    height: 370px
+  @media (min-width: 601px) and (max-width: 800px)
+    height: 108px
+  @media (min-width: 801px) and (max-width: 1000px)
+    height: 140px
+  @media (min-width: 1201px) and (max-width: 1400px)
+    height: 215px
+  @media (min-width: 1401px) and (max-width: 1600px)
+    height: 260px
+  @media (min-width: 1601px)
+    height: 375px
 
 .q-card-actions a:hover
   color: #ff1693 !important
@@ -124,5 +139,5 @@ export default {
   font-family: alenviTitleFont
 
 .q-card-container
-  padding: 16px 0px 0px 16px
+  padding: 6px 0px 0px 16px
 </style>
