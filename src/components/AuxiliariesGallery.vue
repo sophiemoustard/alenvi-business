faeb63b88f7ec9b8c7460c5367a363aa2d022a1d
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-03-15 11:43:21 +0100

    Fix auxiliary picture to reflects changes in db

--- src/components/AuxiliariesGallery.vue
+++ src/components/AuxiliariesGallery.vue
@@ -3,7 +3,7 @@
     <q-window-resize-observable @resize="onResize" />
     <!-- On Mobile -->
     <div class="col-sm-3 auxiliaries-container" v-for="(auxiliary, index) in auxiliaries" v-if="$q.platform.is.mobile && auxiliary.youtube" @click="getVideo($event)" :id="'container-' + (index + 1)">
-      <img :src="auxiliary.picture" alt="splash" />
+      <img :src="auxiliary.picture.link" alt="splash" />
       <div class="auxiliaries-icon-container-mobile" :ref="'video' + (index + 1)">
         <q-video :src="auxiliary.youtube.link" style="width: 100%; height: 100%"/>
 
