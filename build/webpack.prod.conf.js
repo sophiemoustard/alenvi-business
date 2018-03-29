a43a88a243f3c3334edc12631d930e8804c91d3a
Jean-Christophe TREBALAG <jctrebalag@gmail.com>
2018-03-29 12:12:51 +0200

    Remove GTM script from index.html and add it to prerender post process to avoid duplicates

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -86,6 +86,29 @@ module.exports = merge(baseWebpackConfig, {
         ignoreJSErrors: true,
         phantomPageSettings: {
           loadImages: true
+        },
+        postProcessHtml: function (context) {
+          return context.html.replace(
+            /<script>[^<]*<\/script>/i,
+            `<script>
+            (function (w, d, s, l, i) {
+              if (window._hasGTM) return;
+              w[l] = w[l] || [];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+              });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+              j.async = true;
+              j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+              f.parentNode.insertBefore(j, f);
+              window._hasGTM = true;
+            })(window, document, 'script', 'dataLayer', 'GTM-5MT8HT');
+            </script>`
+          )
         }
         // postProcessHtml: function(context) {
         //   var contents = {
